1447aafb28f145d0abceefca833de1318648415c
fdsdsfdsf <NestZ671@gmail.com>
2023-12-05 14:44:17 +0700

    add more doc

--- src/Equipments/Equipment.java
+++ src/Equipments/Equipment.java
@@ -3,6 +3,7 @@ package Equipments;
 import Characters.Character;
 import Equipments.Interface.Accessory;
 
+import java.util.HashMap;
 import java.util.Random;
 
 public class Equipment implements Accessory {
@@ -41,21 +42,35 @@ public class Equipment implements Accessory {
         this.level++;
     }
 
+    /**Combine 2 Equipment and Return new Equipment
+     @param eq1 Equipment1
+     @param eq2 Equipment1
+     @return random equipment after combine 2 equipment
+     */
     public Equipment combine(Equipment eq1, Equipment eq2) {
 
         if (eq1 == null || eq2 == null || equipmentArray == null || equipmentArray.length == 0) {
             return null;
         }
-
         Random random = new Random();
         int randomIndex = random.nextInt(equipmentArray.length);
-
         return equipmentArray[randomIndex];
     }
 
-    public void recycle(Character c) {
-        int exp = c.getExp();
-        c.setExp(exp+=10);
+    /**Combine 2 Equipment and Return new Equipment
+     @param c Character that recycle
+     @param type weapon location for recycle
+     effects: character exp += 10
+     effects: weapon location for character change to null
+     */
+    public void recycle(Character c , Character.PartType type) {
+        HashMap<Character.PartType,Equipment> eqMap =  c.getEquipmentMap();
+        Equipment equipment = eqMap.get(type);
+        if(equipment != null) {
+            int exp = c.getExp();
+            c.setExp(exp+=10);
+            eqMap.put(type, equipment);
+        }
     }
 
 }
